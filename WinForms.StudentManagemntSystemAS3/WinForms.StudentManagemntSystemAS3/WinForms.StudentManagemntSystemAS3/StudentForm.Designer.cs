669fd4bcb0856224cdfcb205051ff23c4c8e8472
Siddharth <n01616739@humber.ca>
2024-03-29 16:26:53 -0400

    Assignment3

---
+++ WinForms.StudentManagemntSystemAS3/WinForms.StudentManagemntSystemAS3/WinForms.StudentManagemntSystemAS3/StudentForm.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace WinForms.StudentManagemntSystemAS3
+{
+    partial class StudentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addbtn = new System.Windows.Forms.Button();
+            this.editbtn = new System.Windows.Forms.Button();
+            this.deletebtn = new System.Windows.Forms.Button();
+            this.exitbtn = new System.Windows.Forms.Button();
+            this.studentGridView = new System.Windows.Forms.DataGridView();
+            this.StudentID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FirstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Age = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Gender = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Class = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Grade = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.searchbtn = new System.Windows.Forms.Button();
+            this.searchtxt = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.studentGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // addbtn
+            // 
+            this.addbtn.Location = new System.Drawing.Point(74, 350);
+            this.addbtn.Name = "addbtn";
+            this.addbtn.Size = new System.Drawing.Size(75, 23);
+            this.addbtn.TabIndex = 1;
+            this.addbtn.Text = "Add";
+            this.addbtn.UseVisualStyleBackColor = true;
+            this.addbtn.Click += new System.EventHandler(this.addbtn_Click);
+            // 
+            // editbtn
+            // 
+            this.editbtn.Location = new System.Drawing.Point(231, 350);
+            this.editbtn.Name = "editbtn";
+            this.editbtn.Size = new System.Drawing.Size(75, 23);
+            this.editbtn.TabIndex = 2;
+            this.editbtn.Text = "Edit";
+            this.editbtn.UseVisualStyleBackColor = true;
+            this.editbtn.Click += new System.EventHandler(this.editbtn_Click);
+            // 
+            // deletebtn
+            // 
+            this.deletebtn.Location = new System.Drawing.Point(386, 350);
+            this.deletebtn.Name = "deletebtn";
+            this.deletebtn.Size = new System.Drawing.Size(75, 23);
+            this.deletebtn.TabIndex = 3;
+            this.deletebtn.Text = "Delete";
+            this.deletebtn.UseVisualStyleBackColor = true;
+            this.deletebtn.Click += new System.EventHandler(this.deletebtn_Click);
+            // 
+            // exitbtn
+            // 
+            this.exitbtn.Location = new System.Drawing.Point(551, 350);
+            this.exitbtn.Name = "exitbtn";
+            this.exitbtn.Size = new System.Drawing.Size(75, 23);
+            this.exitbtn.TabIndex = 4;
+            this.exitbtn.Text = "Exit";
+            this.exitbtn.UseVisualStyleBackColor = true;
+            this.exitbtn.Click += new System.EventHandler(this.exitbtn_Click);
+            // 
+            // studentGridView
+            // 
+            this.studentGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.studentGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.StudentID,
+            this.FirstName,
+            this.LastName,
+            this.Age,
+            this.Gender,
+            this.Class,
+            this.Grade});
+            this.studentGridView.Location = new System.Drawing.Point(20, 53);
+            this.studentGridView.Name = "studentGridView";
+            this.studentGridView.RowHeadersWidth = 51;
+            this.studentGridView.RowTemplate.Height = 24;
+            this.studentGridView.Size = new System.Drawing.Size(1155, 257);
+            this.studentGridView.TabIndex = 0;
+            // 
+            // StudentID
+            // 
+            this.StudentID.HeaderText = "StudentID";
+            this.StudentID.MinimumWidth = 6;
+            this.StudentID.Name = "StudentID";
+            this.StudentID.Width = 125;
+            // 
+            // FirstName
+            // 
+            this.FirstName.HeaderText = "FirstName";
+            this.FirstName.MinimumWidth = 6;
+            this.FirstName.Name = "FirstName";
+            this.FirstName.Width = 125;
+            // 
+            // LastName
+            // 
+            this.LastName.HeaderText = "LastName";
+            this.LastName.MinimumWidth = 6;
+            this.LastName.Name = "LastName";
+            this.LastName.Width = 125;
+            // 
+            // Age
+            // 
+            this.Age.HeaderText = "Age";
+            this.Age.MinimumWidth = 6;
+            this.Age.Name = "Age";
+            this.Age.Width = 125;
+            // 
+            // Gender
+            // 
+            this.Gender.HeaderText = "Gender";
+            this.Gender.MinimumWidth = 6;
+            this.Gender.Name = "Gender";
+            this.Gender.Width = 125;
+            // 
+            // Class
+            // 
+            this.Class.HeaderText = "Class";
+            this.Class.MinimumWidth = 6;
+            this.Class.Name = "Class";
+            this.Class.Width = 125;
+            // 
+            // Grade
+            // 
+            this.Grade.HeaderText = "Grade";
+            this.Grade.MinimumWidth = 6;
+            this.Grade.Name = "Grade";
+            this.Grade.Width = 125;
+            // 
+            // searchbtn
+            // 
+            this.searchbtn.Location = new System.Drawing.Point(1100, 12);
+            this.searchbtn.Name = "searchbtn";
+            this.searchbtn.Size = new System.Drawing.Size(75, 23);
+            this.searchbtn.TabIndex = 6;
+            this.searchbtn.Text = "Search";
+            this.searchbtn.UseVisualStyleBackColor = true;
+            this.searchbtn.Click += new System.EventHandler(this.searchbtn_Click);
+            // 
+            // searchtxt
+            // 
+            this.searchtxt.Location = new System.Drawing.Point(931, 12);
+            this.searchtxt.Name = "searchtxt";
+            this.searchtxt.Size = new System.Drawing.Size(145, 22);
+            this.searchtxt.TabIndex = 7;
+            // 
+            // StudentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1246, 472);
+            this.Controls.Add(this.searchtxt);
+            this.Controls.Add(this.searchbtn);
+            this.Controls.Add(this.exitbtn);
+            this.Controls.Add(this.deletebtn);
+            this.Controls.Add(this.editbtn);
+            this.Controls.Add(this.addbtn);
+            this.Controls.Add(this.studentGridView);
+            this.Name = "StudentForm";
+            this.Text = "StudentForm";
+            this.Load += new System.EventHandler(this.StudentForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.studentGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button addbtn;
+        private System.Windows.Forms.Button editbtn;
+        private System.Windows.Forms.Button deletebtn;
+        private System.Windows.Forms.Button exitbtn;
+        private System.Windows.Forms.DataGridView studentGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn StudentID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FirstName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LastName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Age;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Gender;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Class;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Grade;
+        private System.Windows.Forms.Button searchbtn;
+        private System.Windows.Forms.TextBox searchtxt;
+    }
+}
---
+++ WinForms.StudentManagemntSystemAS3/WinForms.StudentManagemntSystemAS3/WinForms.StudentManagemntSystemAS3/StudentForm.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForms.StudentManagemntSystemAS3
+{
+    public partial class StudentForm : Form
+    {
+        public StudentForm()
+        {
+            InitializeComponent();
+        }
+
+        private void addbtn_Click(object sender, EventArgs e)
+        {
+            //add student
+            AddStudentForm addStudent = new AddStudentForm(false, null);
+            addStudent.ShowDialog();
+            RefreshGridView();
+        }
+
+        public void RefreshGridView()
+        {
+            // Refresh the grid view with the updated list of students in students.txt which is also in studentlist
+            studentGridView.Rows.Clear();
+            List<Student> studentlist = StudentDB.GetStudents();
+            foreach (Student student in studentlist)
+            {
+                studentGridView.Rows.Add(student.StudentID, student.FirstName, student.LastName, student.Age, student.Gender, student.Class, student.Grade);
+            }
+
+        }
+
+        private void exitbtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void StudentForm_Load(object sender, EventArgs e)
+        {
+            RefreshGridView();
+        }
+
+        private void editbtn_Click(object sender, EventArgs e)
+        {
+            if (studentGridView.SelectedRows.Count > 0)
+            {
+                int selectedIndex = studentGridView.SelectedRows[0].Index;
+                // Open edit form and pass the selected student details
+                Student selectedStudent = StudentDB.GetStudents()[selectedIndex];
+                AddStudentForm addStudent = new AddStudentForm(true, selectedStudent);
+                addStudent.ShowDialog();
+                RefreshGridView();
+            }
+            else
+            {
+                MessageBox.Show("Please select a student to edit");
+            }
+        }
+
+        private void deletebtn_Click(object sender, EventArgs e)
+        {
+            if (studentGridView.SelectedRows.Count > 0)
+            {
+                int selectedIndex = studentGridView.SelectedRows[0].Index;
+                // Delete the selected student
+                StudentDB.DeleteStudent(selectedIndex);
+                RefreshGridView();
+            }
+            else
+            {
+                MessageBox.Show("Please select a student to delete");
+            }
+        }
+
+        private void searchbtn_Click(object sender, EventArgs e)
+        {
+            //Enable searching functionality in the DataGridView dataset by ID, firstname or lastname. Hint use LINQ.
+            string search = searchtxt.Text;
+            List<Student> studentlist = StudentDB.GetStudents();
+            if (string.IsNullOrEmpty(search))
+            {
+                RefreshGridView();
+                return;
+            }
+            else
+            {
+                var searchResult = studentlist.Where(s => s.StudentID.ToString().Contains(search) || s.FirstName.Contains(search) || s.LastName.Contains(search)).ToList();
+                studentGridView.Rows.Clear();
+                foreach (Student student in searchResult)
+                {
+                    studentGridView.Rows.Add(student.StudentID, student.FirstName, student.LastName, student.Age, student.Gender, student.Class, student.Grade);
+                }
+            }
+            
+
+        }
+    }
+}
